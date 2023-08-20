f70e67d63536b23a87703eeb844f610caf7a1219
David G Rosenberg <targendaz2@me.com>
2023-08-21 01:03:57 +0800

    Consolidated basic CLI tests

--- test/package.test.ts
+++ test/package.test.ts
@@ -6,116 +6,46 @@ import decamelize from 'decamelize';
 
 import { bats, BatsOptions } from '../src/index.js';
 
-suite('test selection tests', function () {
-    test('single file test', function () {
-        const batsPath = './fixtures/bats_files/addition.bats';
-        const result = cp.spawnSync('npx bats ' + batsPath, {
+suite('CLI parity tests', function () {
+    const batsCommands: [string, BatsOptions?][] = [
+        ['./fixtures/bats_files/addition.bats'],
+        ['./fixtures/bats_files/'],
+        ['./fixtures/bats_files/', { count: true }],
+        ['./fixtures/bats_files/', { noTempdirCleanup: true }],
+        ['./fixtures/bats_files/', { printOutputOnFailure: true }],
+        ['./fixtures/bats_files/', { recursive: true }],
+        ['./fixtures/bats_files/', { trace: true }],
+        ['./fixtures/bats_files/', { verboseRun: true }],
+        ['./fixtures/bats_files/', { count: true, noTempdirCleanup: true, recursive: true }],
+    ];
+
+    const testBatsCommand = (tests: string, options?: BatsOptions) => function () {
+        let command = `npx bats ${tests}`;
+
+        // parse options into command
+        for (const option in options) {
+            command += ' --' + decamelize(option, { separator: '-' });
+        }
+
+        const result = cp.spawnSync(command, {
             'cwd': '.',
             'shell': true
         });
 
         assert.equal(
-            bats(batsPath).output,
-            result.stdout.toString()
-        );
-    });
-
-    test('single folder test', function () {
-        const batsPath = './fixtures/bats_files/';
-        const result = cp.spawnSync('npx bats ' + batsPath, {
-            'cwd': '.',
-            'shell': true
-        });
-
-        assert.equal(
-            bats(batsPath).output,
-            result.stdout.toString()
-        );
-    });
-
-    test('recursive folder test', function () {
-        const batsPath = './fixtures/bats_files/';
-        const result = cp.spawnSync('npx bats ' + batsPath + ' --recursive', {
-            'cwd': '.',
-            'shell': true
-        });
-
-        assert.equal(
-            bats(batsPath, {recursive: true}).output,
-            result.stdout.toString()
-        );
-    });
-});
-
-suite('tempdir tests', function () {
-    test('return value test', function () {
-        const batsPath = './fixtures/bats_files/';
-        const result = cp.spawnSync('npx bats ' + batsPath + ' --no-tempdir-cleanup --recursive', {
-            'cwd': '.',
-            'shell': true
-        });
-
-        assert.equal(
-            bats(batsPath, {noTempdirCleanup: true, recursive: true}).output,
-            result.stdout.toString()
-        );
-    });
-
-    test.skip('returns path to tempdir', function () {
-        const batsPath = './fixtures/bats_files/';
-        const result = cp.spawnSync('npx bats ' + batsPath + ' --no-tempdir-cleanup --recursive', {
-            'cwd': '.',
-            'shell': true
-        });
-
-        assert.include(
-            result.stdout.toString(),
-            bats(batsPath, {noTempdirCleanup: true, recursive: true}).tempdir as string
-        );
-    });
-});
-
-suite('parameter tests', function () {
-    const batsPath = './fixtures/bats_files/';
-    const options: BatsOptions = {
-        count: true,
-        noTempdirCleanup: true,
-        printOutputOnFailure: true,
-        recursive: true,
-        trace: true,
-        verboseRun: true,
-    };
-
-    const testBoolOption = (option: string) => function () {
-        const cliOption = decamelize(option, { separator: '-' });
-
-        const result = cp.spawnSync(`npx bats ${batsPath} --${cliOption}`, {
-            'cwd': '.',
-            'shell': true
-        });
-
-        const tempOptions: BatsOptions = {};
-        tempOptions[option] = true;
-
-        assert.equal(
-            bats(batsPath, tempOptions).output,
+            bats(tests, options).output,
             result.stdout.toString()
         );
     };
 
-    for (const option in options) {
-        test(`${option} is a valid boolean option`, testBoolOption(option));
-    }
+    batsCommands.forEach(([tests, options]) => {
+        let command = `bats ${tests}`;
 
-    test('count returns number', function () {
-        const result = cp.spawnSync('npx bats ' + batsPath + ' --count', {
-            'cwd': '.',
-            'shell': true
-        });
+        // parse options into command
+        for (const option in options) {
+            command += ' --' + decamelize(option, { separator: '-' });
+        }
 
-        assert.equal(
-            bats(batsPath, { count: true }).output,
-            parseInt(result.stdout.toString())
-        );
+        test(`parity for "${command}"`, testBatsCommand(tests, options));
     });
 });
