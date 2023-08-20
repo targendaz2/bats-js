c195510fbf671d13a4b4c2f2c21cd23d51fb9028
David G Rosenberg <targendaz2@me.com>
2023-08-21 02:55:09 +0800

    Implemented most Bats options

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,42 @@ export interface BatsOptions {
     count?: boolean;
     /** Only run tests that match the regular expression */
     filter?: string;
+    /** 
+     * Only run tests with the given status in the last completed (no CTRL+C/SIGINT) run.
+     * 
+     * Valid <status> values are:
+     * * failed - runs tests that failed or were not present in the last run
+     * * missed - runs tests that were not present in the last run
+     * */
+    filterStatus?: 'failed' | 'missed';
+    /** 
+     * Only run tests that match all the tags in the list (&&).
+     * 
+     * You can negate a tag via prepending '!'.
+     * */
+    filterTags?: string;
+    /** Switch between formatters: pretty (default), tap (default w/o term), tap13, junit */
+    formatter?: 'pretty' | 'tap' | 'tap13' | 'junit';
+    /** Number of parallel jobs (requires GNU parallel or shenwei356/rush) */
+    jobs?: number;
+    /** Name of parallel binary */
+    parallelBinaryName?: string;
     /** Preserve test output temporary directory */
     noTempdirCleanup?: boolean;
+    /** Serialize test file execution instead of running them in parallel (requires jobs >1) */
+    noParallelizeAcrossFiles?: boolean;
+    /** Serialize test execution within files instead of running them in parallel (requires jobs >1) */
+    noParallelizeWithinFiles?: boolean;
+    /** Shorthand for "formatter pretty" */
+    pretty?: boolean;
     /** Automatically print the value of `$output` on failed tests */
     printOutputOnFailure?: boolean;
     /** Include tests in subdirectories */
     recursive?: boolean;
-    /** Add timing information to tests */
-    timing?: boolean;
+    /** Print output of passing tests */
+    showOutputOfPassingTests?: boolean;
+    /** Shorthand for "formatter tap" */
+    tap?: boolean;
     /** Print test commands as they are executed (like `set -x`) */
     trace?: boolean;
     /** Make `run` print `$output` by default */
@@ -40,16 +68,40 @@ export function bats(tests: string = '.', options?: BatsOptions): BatsResult {
         command += ` --filter ${options.filter}`;
     }
 
+    if (options?.filterStatus) {
+        command += ` --filter-status ${options.filterStatus}`;
+    }
+
+    if (options?.filterTags) {
+        command += ` --filter-tags ${options.filterTags}`;
+    }
+
+    if (options?.formatter) {
+        command += ` --formatter ${options.formatter}`;
+    }
+
+    if (options?.jobs) {
+        command += ` --jobs ${options.jobs}`;
+    }
+
     if (options?.noTempdirCleanup) {
         command += ' --no-tempdir-cleanup';
     }
 
-    if (options?.recursive) {
-        command += ' --recursive';
+    if (options?.noParallelizeAcrossFiles) {
+        command += ' --no-parallelize-across-files';
     }
 
-    if (options?.timing) {
-        command += ' --timing';
+    if (options?.noParallelizeWithinFiles) {
+        command += ' --no-parallelize-within-files';
+    }
+
+    if (options?.pretty) {
+        command += ' --pretty';
+    }
+
+    if (options?.recursive) {
+        command += ' --recursive';
     }
 
     const result = cp.spawnSync(command, {
